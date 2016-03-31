8190e1702d52c76ca412a3d0d64f54520eff26c3
Aaron Haaf <aabonh@gmail.com>
2016-03-31 01:07:21 -0700

    Dynamically generate DataDragon info and endpoints.

--- src/main.js
+++ src/main.js
@@ -6,15 +6,6 @@ import $ from 'jquery';
 // Things to do
 import ChampionPool from './app/ChampionPool'
 
-var dd = {
-	available_realms: ['br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'tr', 'ru', 'jp'],
-	version: localStorage.getItem('dd_version') || '6.5.1',
-	language: localStorage.getItem('dd_language') || 'en_US',
-	realm: localStorage.getItem('dd_realm') || 'na',
-	cdn: localStorage.getItem('dd_cdn') || 'http://ddragon.leagueoflegends.com/cdn'
-}
-
-console.log(dd)
 
 // ID of the DOM element to mount app on
 const DOM_APP_EL_ID = 'app';
@@ -22,16 +13,29 @@ const DOM_APP_EL_ID = 'app';
 let championData = null
 let itemData = null
 let languageData = null
+let realmData = null
+let dd = null
 
-$.when(
-  $.getJSON(`json/${dd.language}/item.json`,     (data => itemData = data)),
-  $.getJSON(`json/${dd.language}/champion.json`, (data => championData = data)),
-  $.getJSON(`json/${dd.language}/language.json`, (data => languageData = data))
-).then(function(x) {
-	console.log("%o", dd);
-
-	ReactDOM.render(
-		<ChampionPool champions={championData.data} dataDragon={dd} />, 
-		document.getElementById(DOM_APP_EL_ID)
-	);
-});
+$.getJSON(`json/realm_${localStorage.getItem('dd_realm') || 'na'}.json`, (data => realmData = data))
+ .then(function(x) {
+	dd = {
+		available_realms: ['br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'tr', 'ru', 'jp'],
+		cdn: realmData.cdn,
+		version: realmData.dd,
+		language: localStorage.getItem('dd_language') || realmData.l,
+		version: realmData.v
+	};
+ })
+ .then(function(x) {
+	$.when(
+		$.getJSON(`json/${dd.language}/item.json`,     (data => itemData = data)),
+		$.getJSON(`json/${dd.language}/champion.json`, (data => championData = data)),
+		$.getJSON(`json/${dd.language}/language.json`, (data => languageData = data))
+	)
+	.then(function(x) {
+		ReactDOM.render(
+			<ChampionPool champions={championData.data} dataDragon={dd} />,
+			document.getElementById(DOM_APP_EL_ID)
+		);
+	});
+ });
