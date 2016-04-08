897f7e583af9480fb55607df4ca8106d7fac4ec3
Aaron Haaf <aabonh@gmail.com>
2016-04-08 09:08:00 -0700

    I forgot that this was not a react component
    I am an idiot

--- src/app/UltimateBravery.js
+++ src/app/UltimateBravery.js
@@ -8,130 +8,124 @@ import ItemIcon from '../lol/item/ItemIcon'
 
 class UltimateBravery {
 
-    constructor() {
-        this.render = this.render.bind(this);
-        this.rerender = this.rerender.bind(this);
-        this.updateDataDragon = this.updateDataDragon.bind(this);
-        this.saveUser = this.saveUser.bind(this);
-        this.loadUser = this.loadUser.bind(this);
+  constructor() {
+    this.updateDataDragon(null, null);
+  }
 
-        this.championData = {};
-        this.itemData = {};
-        this.languageData = {};
-        this.dd = {}
-        this.user = this.loadUser();
-        this.updateDataDragon(null, null);
+  saveUser() {
+    new StorageManager().saveObject('user', this.state.user)
+  }
+
+  loadUser() {
+    let defaultUser = {
+      championData: {},   //YOU HAVE NOTHING
+      itemData: {},
+      gameMode: 1,        //Is summoner's rift
+      summonerLevel: 30,
+      firstVisit: true
     }
 
-    saveUser() {
-      new StorageManager().saveObject('user', this.user)
-    }
-
-    loadUser() {
-      let defaultUser = {
-          championData: {},   //YOU HAVE NOTHING
-          itemData: {},
-          gameMode: 1,        //Is summoner's rift
-          summonerLevel: 30
+    if(user.firstVisit) {
+      for(let key in this.state.championData.data) {
+        user.championData[key] = true
+        console.log(key)
       }
-      let user = new StorageManager().loadObject('user', defaultUser)
-      return user
+      //user.firstVisit = false
     }
 
-    toggleChampion(champion) {
+    let user = new StorageManager().loadObject('user', defaultUser)
 
+    return user
+  }
+
+  toggleChampion(champion) {
+    let state = this.state.user
+    if(state.championData[champion]) {
+      state.championData[champion] = false
+    } else {
+      state.championData[champion] = true
+    }
+    this.saveUser()
+    this.render()
+  }
+
+  render() {
+    let style = {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center'
     }
 
-    render() {
-      let style = {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center'
-      }
-
-      ReactDOM.render(
-        <div>
-          <MainDisplay
-            championData={this.championData}
-            itemData={this.itemData}
-            userData={this.userData}
-            dd={this.dd}
-          />
-          <br/>
-          <ChampionPool
-            userChampions={this.user.championData}
+    ReactDOM.render(
+      <div>
+        <MainDisplay
+          user={this.state.user}
           championData={this.championData}
+          itemData={this.itemData}
+          userData={this.userData}
           dd={this.dd}
-          />
+        />
+        <br/>
+        <ChampionPool
+          userChampionData={this.state.user.championData}
+          championData={this.championData}
+          toggleChampion={this.toggleChampion.bind(this)}
+          dd={this.dd}
+        />
+      </div>,
+      document.getElementById('app')
+    );
+  }
 
-          <h3>ALL THE ITEMS</h3>
-          <div style={style}>
-            {Object.keys(this.itemData.data).map( (item) =>
-              <ItemIcon key={this.itemData.data[item].key} image={this.itemData.data[item].image} dd={this.dd}/>
-            )}
-          </div>
-          <h3>BOOTS</h3>
-          <div style={style}>
-            {this.itemData.lists.boots.map( (item) =>
-              <ItemIcon key={this.itemData.data[item].key} image={this.itemData.data[item].image} dd={this.dd}/>
-            )}
-          </div>
-          <h3>ANYBODY (Any map)</h3>
-          <div style={style}>
-            {this.itemData.lists.generics.map( (item) =>
-              <ItemIcon key={this.itemData.data[item].key} image={this.itemData.data[item].image} dd={this.dd}/>
-            )}
-          </div>
-          <h3>JANGLE (Not implemented yet)</h3>
-          <div style={style}>
-            {this.itemData.lists.jungleItems.map( (item) =>
-              <ItemIcon key={this.itemData.data[item].key} image={this.itemData.data[item].image} dd={this.dd}/>
-            )}
-          </div>
-        </div>,
-        document.getElementById('app')
-      );
-    }
+  updateDataDragon(realm, language) {
+    let selectedRealm = realm || localStorage.getItem('dd_realm') || 'na';
+    localStorage.setItem('dd_realm', selectedRealm);
+    let realmData = null;
+    let reallyThis = this
 
-    rerender() {
+    var dd = {}
+    var itemData = {}
+    var championData = {}
+    var languageData = {}
 
+    $.getJSON(`json/realm_${selectedRealm}.json`, (data => realmData = data))
+     .then(()=>{
+        let selectedLanguage = language || realmData.l || localStorage.getItem('dd_language') || 'en_US'
+        localStorage.setItem('dd_language', selectedLanguage)
+
+        dd = {
+          available_realms: ['br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'tr', 'ru', 'jp'],
+          cdn: realmData.cdn,
+          version: realmData.dd,
+          language: selectedLanguage,
+          version: realmData.v
+        }
+    }).then(() => {
       let itemUpdated = $.getJSON(
-          `json/${this.dd.language}/item.json`,
-          (data => this.itemData = data)
+        `json/${dd.language}/item.json`,
+        (data => itemData = data)
       );
       let championUpdated = $.getJSON(
-          `json/${this.dd.language}/champion.json`,
-          (data => this.championData = data)
+        `json/${dd.language}/champion.json`,
+        (data => championData = data)
       );
       let languageUpdated = $.getJSON(
-          `json/${this.dd.language}/language.json`,
-          (data => this.languageData = data)
+        `json/${dd.language}/language.json`,
+        (data => languageData = data)
       );
 
-      $.when(itemUpdated, championUpdated, languageUpdated).then(this.render);
-    }
-
-    updateDataDragon(realm, language) {
-        let selectedRealm = realm || localStorage.getItem('dd_realm') || 'na';
-        localStorage.setItem('dd_realm', selectedRealm);
-        let realmData = null;
-
-        let reallyThis = this
-
-        $.getJSON(`json/realm_${selectedRealm}.json`, (data => realmData = data))
-         .then(function(x) {
-            let selectedLanguage = language || realmData.l || localStorage.getItem('dd_language') || 'en_US'
-            localStorage.setItem('dd_language', selectedLanguage)
-
-            reallyThis.dd = {
-                available_realms: ['br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'tr', 'ru', 'jp'],
-                cdn: realmData.cdn,
-                version: realmData.dd,
-                language: selectedLanguage,
-                version: realmData.v
-            };
-        }).then(this.rerender);
-    }
+      $.when(itemUpdated, championUpdated, languageUpdated)
+       .then(() => {
+          reallyThis.setState({
+            dd: dd,
+            itemData: itemData,
+            championData: championData,
+            languageData: languageData,
+            user: reallyThis.loadUser()
+          })
+       });
+    });
+  }
 }
 
 export default UltimateBravery;
