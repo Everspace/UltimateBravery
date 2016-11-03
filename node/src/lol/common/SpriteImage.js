3a022118c621c22bacf65d4fe080784082465667
Aaron Haaf <aabonh@gmail.com>
2016-11-03 00:48:59 -0700

    Everything that comes out of a image blob is SpriteImage now. Need to rework how images are classified.

--- node/src/lol/common/SpriteImage.js
+++ node/src/lol/common/SpriteImage.js
@@ -2,21 +2,25 @@ import React, { PropTypes } from 'react';
 
 class SpriteImage extends React.Component {
 
+  render() {
+    let url = `url('${this.props.dd.cdn}/${this.props.dd.version}/img/sprite/${this.props.image.sprite}')`
 
-    render() {
-    	let dd = this.props.dd
-
-    	let elementStyle = {
-		    width: this.props.image.w,
-		    height: this.props.image.h,
-		    background: `url('${dd.cdn}/${dd.version}/img/sprite/${this.props.image.sprite}')`,
-		    backgroundPositionX: -this.props.image.x,
-		    backgroundPositionY: -this.props.image.y,
-		    backgroundRepeat: "no-repeat"
-    	};
-
-    	return <div className='SpriteImage' style={elementStyle}></div>;
+    let style = {
+      width: this.props.image.w,
+      height: this.props.image.h,
+      //Currently not using backgroundPositionX or Y because it sometimes
+      //doesn't set the style attribute on the div correctly
+      background: `${url} ${-this.props.image.x}px ${-this.props.image.y}px`,
+      backgroundRepeat: "no-repeat"
     }
+
+    return <div
+      className={this.props.className}
+      onClick={this.props.onClick}
+      name={this.props.name}
+      style={style}
+    />;
+  }
 }
 
 SpriteImage.propTypes = {
