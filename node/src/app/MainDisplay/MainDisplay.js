78b74232d3edd321e3182e76733ca2eb2d143025
Aaron Haaf <aabonh@gmail.com>
2016-11-11 01:45:31 -0800

    Add sweet background and tooltips to MainDisplay.

--- node/src/app/MainDisplay/MainDisplay.js
+++ node/src/app/MainDisplay/MainDisplay.js
@@ -7,6 +7,8 @@ import ChampionTitle from 'lol/champion/ChampionTitle'
 import ChampionIcon from 'lol/champion/ChampionIcon'
 import SpriteImage from 'lol/common/SpriteImage'
 
+import './MainDisplay.less'
+
 export default class MainDisplay extends React.Component {
 
   constructor(props) {
@@ -28,50 +30,48 @@ export default class MainDisplay extends React.Component {
   }
 
   render() {
-
-    let containerStyle = {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'left'
+    let type = (window.matchMedia("(min-width: 426px)").matches) ? "splash" : "loading"
+    let style = {
+      backgroundImage: `url("${dd.cdn}/img/champion/${type}/${this.state.brave.champion.id}_0.jpg")`
     }
-
     return (
-      <div className="MainDisplay">
-        <ChampionTitle
-          champion={this.state.brave.champion}
-          dd={this.props.dd}
-        />
-        <button
-          onClick={()=>this.setState({brave: this.braveFactory.makeBrave()})}
-        >BRAVERY!</button>
-        <h3>{this.props.languageData.data.RecommendedItems}</h3>
-        <div style={containerStyle}>
-          {this.state.brave.items.map((item)=>{
-            return <SpriteImage
-                      name={item.name}
-                      key={item.name}
-                      image={item.image}
-                      dd={this.props.dd}
-                    />
-          })}
-        </div>
-        {this.state.brave.extraItems.length ?
-          <div>
-            <h3>{this.props.languageData.data.Details_}</h3>
-            <div style={containerStyle}>
-              {this.state.brave.extraItems.map((item)=>{
-                return <SpriteImage
-                      name={item.name}
-                      key={item.key}
-                      image={item.image}
-                      dd={this.props.dd}
-                    />
-              })}
-            </div>
+      <div className="MainDisplay" style={style}>
+        <div className="Content">
+          <ChampionTitle
+            champion={this.state.brave.champion}
+            dd={this.props.dd}
+          />
+          <button
+            onClick={()=>this.setState({brave: this.braveFactory.makeBrave()})}
+          >BRAVERY!</button>
+          <h3>{this.props.languageData.data.RecommendedItems}</h3>
+          <div className="SpriteRow">
+            {this.state.brave.items.map((item)=>{
+              return <SpriteImage
+                        name={item.name}
+                        key={item.name}
+                        image={item.image}
+                        dd={this.props.dd}
+                      />
+            })}
           </div>
-          :null
-        }
-
+          {this.state.brave.extraItems.length ?
+            <div>
+              <h3>{this.props.languageData.data.Details_}</h3>
+              <div className="SpriteRow">
+                {this.state.brave.extraItems.map((item)=>{
+                  return <SpriteImage
+                        name={item.name}
+                        key={item.key}
+                        image={item.image}
+                        dd={this.props.dd}
+                      />
+                })}
+              </div>
+            </div>
+            :null
+          }
+        </div>
       </div>
     )
   }
