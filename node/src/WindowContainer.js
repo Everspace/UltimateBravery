595516b9e41993b9bc4c34b752b38008e54328fc
Everspace <aabonh@gmail.com>
2017-05-18 00:34:32 -0700

    Try to get hotreloading going again

--- node/src/WindowContainer.js
+++ node/src/WindowContainer.js
@@ -4,29 +4,25 @@ import { Provider } from 'react-redux'
 import * as displays from 'Displays'
 import './WindowContainer.less'
 
-const WindowContainer = () => {
-  console.log(displays)
-  let Window = displays.ChampionPool.MainWindow
-  let initialState = {
-    champions: window.dat.champions.allChampions.reduce((memory, id) => { memory[id] = true; return memory }, {})
-  }
-
-  let store = createStore(
+export default class WindowContainer extends React.Component {
+  store = createStore(
     displays.ChampionPool.reducer,
-    initialState
+    {
+      champions: window.dat.champions.allChampions.reduce((memory, id) => { memory[id] = true; return memory }, {})
+    }
   )
 
-  return (
-    <Provider store={store}>
+  render () {
+    let Window = displays.ChampionPool.MainWindow
+
+    return <Provider store={this.store}>
       <div className='WindowContainer'>
-        <div className='Header'>Hello</div>
+        <div className='Header'>HeEElo?</div>
         <div className='Content'>
           <Window />
         </div>
         <div className='Footer'>Hello</div>
       </div>
     </Provider>
-  )
+  }
 }
-
-export default WindowContainer
--- node/webpack/webpack.config.dev.js
+++ node/webpack/webpack.config.dev.js
@@ -5,23 +5,43 @@ var DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  devtool: 'source-map',
-  entry: ['whatwg-fetch', 'webpack-hot-middleware/client', './src/main.js'],
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+    'webpack-dev-server/client?http://localhost:9001',
+
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+    'webpack/hot/only-dev-server',
+
+    // activate HMR for React
+    'react-hot-loader/patch',
+
+
+    // App
+    './src/main.js'
+  ],
   output: {
     path: path.join(__dirname, '..', 'build'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/static/'
   },
   module: {
     rules: require('./webpack.loaders.js')
   },
+
   plugins: [
+    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+    new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
+    new webpack.NoEmitOnErrorsPlugin(), // do not emit compiled assets that include errors
     new ExtractTextPlugin('style.css'),
+
     new webpack.DefinePlugin({
       'environment': '"development"',
       NODE_ENV: JSON.stringify('development')
     }),
-    new webpack.HotModuleReplacementPlugin(),
+
     new HtmlWebpackPlugin({
       title: 'ULTIMATE BRAVERY!!!',
       template: path.join('src', 'public', 'index.ejs')
