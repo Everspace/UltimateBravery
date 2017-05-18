a7ac0a404e3a83ab52dafcb188b12cda44394b83
Everspace <aabonh@gmail.com>
2017-05-18 01:58:55 -0700

    pull back the middleware

--- node/server.js
+++ node/server.js
@@ -1,23 +1,34 @@
 var path = require('path')
+var express = require('express')
 
+var app = express()
 var webpack = require('webpack')
 var config = require('./webpack/webpack.config.dev.js')
+var compiler = webpack(config)
+app.use(require('connect-history-api-fallback')({verbose: false}))
 
-var WebpackDevServer = require('webpack-dev-server')
+app.use(
+  require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  })
+// In case of emergencies
+// ,require('body-parser').json()
+)
+app.use(require('webpack-hot-middleware')(compiler))
 
-new WebpackDevServer(webpack(config),
-  {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    stats: {
-      colors: true
-    }
-  }
-).listen(9001, '0.0.0.0', function (err) {
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src', 'public', 'index.ejs'))
+})
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'static', req.originalUrl))
+})
+
+var server = app.listen(9001, '0.0.0.0', function (err) {
   if (err) {
     console.log(err)
+    return
   }
-
-  console.log('Listening at localhost:9001')
+  console.log('Listening at http://localhost:%d', server.address().port)
 })
