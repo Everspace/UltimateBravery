33f9844d276cdaa6f2d69af446c343eb2f796cb0
Everspace <7560635+Everspace@users.noreply.github.com>
2019-12-16 19:04:03 -0800

    Extract possibly reused styles elsewhere

---
+++ src/style/Util.ts
@@ -0,0 +1,14 @@
+import { CSSObject } from "@emotion/css"
+
+export const championSplashBackground: CSSObject = {
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center center",
+  backgroundAttachment: "fixed",
+  backgroundSize: "cover",
+}
+
+export const fullHeight: CSSObject = {
+  width: "100vw",
+  height: "100vh",
+  minHeight: "100vh",
+}
