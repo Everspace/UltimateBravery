a062f5d7ad87d70dbcc3e4e5f8bd9854ecd2c268
Everspace <7560635+Everspace@users.noreply.github.com>
2019-12-16 19:04:12 -0800

    add type

--- src/style/Borders.ts
+++ src/style/Borders.ts
@@ -1,6 +1,7 @@
+import { CSSObject } from "@emotion/css"
 import { goldLighter, gold } from "style/Colors"
 
-export const goldenHighlightBorder = {
+export const goldenHighlightBorder: CSSObject = {
   borderWidth: 1,
   borderStyle: "solid",
   borderTopColor: goldLighter,
